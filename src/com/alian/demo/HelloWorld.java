d5a2c23f8776808be42dcc12cb058cb1d6d3e454
王朝刚 <wangchaochina@126.com>
2017-05-15 23:28:32 +0800

    Initial commit

---
+++ src/com/alian/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.alian.demo;
+
+/**
+ * Created by wchg on 2016/7/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
